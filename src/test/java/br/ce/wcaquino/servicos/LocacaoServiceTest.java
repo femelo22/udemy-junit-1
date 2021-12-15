189313cb6765504c204056720a13b9de1f301f01
luiz fernando <luiz123jfmg@gmail.com>
2021-12-15 11:56:05 -0300

    Utilizando o ErrorCollector
    ErrorCollector nos permite capturar mais de uma falha no teste de uma vez.
    Ele mostra todos os pontos que nossa aplicação falhou.

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,23 +1,28 @@
 package br.ce.wcaquino.servicos;
 
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.is;
+
 import java.util.Date;
 
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.servicos.LocacaoService;
-import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
-	public void teste() {
+	public void testeLocacao() {
 		
 		//cenario
-		
 		LocacaoService service = new LocacaoService();
 		Usuario usuario1 = new Usuario("Usuário 1");
 		Filme filme1 = new Filme("Matrix", 5, 5.00);
@@ -26,9 +31,13 @@ public class LocacaoServiceTest {
 		Locacao locacao = service.alugarFilme(usuario1, filme1);
 		
 		//resultado
-		Assert.assertEquals(5.0, locacao.getValor(), 0.01);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		//assertThat(locacao.getValor(), is(5.0));
+		//assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		//assertTrue(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)));
+		
+		error.checkThat(locacao.getValor(), is(6.0));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(false));
 		
 	}
 
