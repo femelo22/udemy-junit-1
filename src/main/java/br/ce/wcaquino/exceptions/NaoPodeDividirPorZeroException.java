bf4474169a1da12853b460488875cd511469385f
luiz fernando <luiz123jfmg@gmail.com>
2021-12-15 21:52:38 -0300

    Primeira aula TDD
    Criando os testes de uma calculadora para depois implementar o código;
    
    Processo chamdo de RED -> GREEN -> REFECTOR

---
+++ src/main/java/br/ce/wcaquino/exceptions/NaoPodeDividirPorZeroException.java
@@ -0,0 +1,11 @@
+package br.ce.wcaquino.exceptions;
+
+public class NaoPodeDividirPorZeroException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public NaoPodeDividirPorZeroException(String msg) {
+		super(msg);
+	}
+	
+}
+
---
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -0,0 +1,72 @@
+package br.ce.wcaquino.servicos;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
+
+public class CalculadoraTest {
+	
+	private Calculadora calc;
+	
+	@Before
+	public void setup() {
+		calc = new Calculadora();
+	}
+	
+	@Test
+	public void test_DeveSomarDoisValores() {
+		//cenario
+		int a = 5;
+		int b = 3;
+		
+		//acao
+		int resultado = calc.somar(a, b);
+		
+		//verificacao
+		assertEquals(8, resultado);
+	}
+	
+	
+	@Test
+	public void test_DeveSubtrairDoisNumeros() {
+		//cenario
+		int a = 5;
+		int b = 5;
+		
+		//acao
+		int resultado = calc.subtrair(a, b);
+		
+		//verificacao
+		assertEquals(0, resultado);
+	}
+	
+	
+	@Test
+	public void test_DeveDividirDoisValores() throws NaoPodeDividirPorZeroException {
+		//cenario
+		int a = 6;
+		int b = 3;
+	
+		//acao
+		int resultado = calc.dividir(a,b);
+		
+		//verificao
+		assertEquals(2, resultado);
+	}
+	
+	@Test(expected = NaoPodeDividirPorZeroException.class)
+	public void test_DeveLancarExcecaoDividirPorZero() throws NaoPodeDividirPorZeroException {
+		int a = 10;
+		int b = 0;
+		
+		calc.dividir(a, b);
+	}
+	
+	
+	
+
+}
